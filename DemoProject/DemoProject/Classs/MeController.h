2ff5952b33fd0f9238be8758f0786ccbc4d18b46
champ <1275669107@qq.com>
2020-01-20 10:05:32 +0800

    增加了我的

---
+++ DemoProject/DemoProject/Classs/MeController.h
@@ -0,0 +1,17 @@
+//
+//  MeController.h
+//  DemoProject
+//
+//  Created by champ on 2020/1/20.
+//  Copyright © 2020 champ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MeController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ DemoProject/DemoProject/Classs/MeController.m
@@ -0,0 +1,33 @@
+//
+//  MeController.m
+//  DemoProject
+//
+//  Created by champ on 2020/1/20.
+//  Copyright © 2020 champ. All rights reserved.
+//
+
+#import "MeController.h"
+
+@interface MeController ()
+
+@end
+
+@implementation MeController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.title = @"我的";
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
