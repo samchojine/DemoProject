a1987fdea134d63e81750604c1e99f8963bfd03a
champ <1275669107@qq.com>
2020-01-20 10:21:21 +0800

    加了个基类

---
+++ DemoProject/DemoProject/Base/BaseController.m
@@ -0,0 +1,33 @@
+//
+//  BaseController.m
+//  DemoProject
+//
+//  Created by champ on 2020/1/20.
+//  Copyright © 2020 champ. All rights reserved.
+//
+
+#import "BaseController.h"
+
+@interface BaseController ()
+
+@end
+
+@implementation BaseController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.title = @"基类";
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
